e75d97cb451e9675e59248dc0a0ae41cc472013d
raurir <raurir@gmail.com>
2023-11-13 14:57:58 +1100

    move to /games/

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-yarn build --base=https://www.esquemedia.com/airtasker-404
-aws s3 sync ./dist s3://www.esquemedia.com/airtasker-404/
+yarn build --base=https://www.esquemedia.com/games/airtasker-404
+aws s3 sync ./dist s3://www.esquemedia.com/games/airtasker-404/
